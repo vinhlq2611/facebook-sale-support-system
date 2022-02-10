30ffd8a7cb02a257bf087ba5daf9b5d0d4a959d6
unknown <longtennis@gmail.com>
2022-02-10 23:34:31 +0700

    Them update profile

--- controller/controller_users.js
+++ controller/controller_users.js
@@ -1,73 +1,144 @@
-const { UserService } = require('../services')
-const { logError, logWarn } = require('../utils/index')
-const jwt = require('jsonwebtoken')
-const UserController =
-{
-    async login(req, res) {
-        try {
-            // Nhận dữ liệu từ request
-            let username = req.body.username;
-            let password = req.body.password;
-            console.log(req.body)
-            let account = await UserService.find({ username: username, password: password })
-            if (account.length == 1) {
-                let user = account[0];
-                let token = jwt.sign({ username: user.username, password: user.password, type: user.type }, process.env.SECRET_KEY);
-                res.cookie('token', token, { maxAge: 900000, httpOnly: true });
-                return res.json({ data: account[0], message: "Login success" })
-            } else {
-                logWarn("Login fail", { username: username, password: password })
-                return res.json({ data: null, message: "Account not found" })
-            }
-        } catch (error) {
-            console.log("Login ERROR: ", error)
-            return res.json({ data: error, message: "Login Error" })
-        }
-    },
-    async register(req, res) {
-        try {
-            let username = req.body.username;
-            let password = req.body.password;
-            let rePassword = req.body.rePassword;
-            let email = req.body.email;
-            let phone = req.body.phone;
-            let birthday = req.body.birthday;
-            // CHECK EMPTY INPUT  ""
-            if (!username || !password || !rePassword || !email || !phone || !birthday) {
-                console.log("Invalid information", { username, password, rePassword, email, phone })
-                return res.json({ data: null, message: "Invalid information" })
-            } else
-                // CHECK PASSWORD & REPASSWORD
-                if (password != rePassword) {
-                    logWarn("Password not match", { username, password, rePassword, email, phone, birthdate: birthday })
-                    return res.json({ data: null, message: "Password not match" })
-                }
-            // CHECK DUPLICATE ACCOUNT
-            let account = await UserService.find({ username });
-            console.log("Account Found: ", account)
-            if (account.length != 0) {
-                logWarn("Account Is Existed", { username, password, rePassword, email, phone, birthdate: birthday })
-                return res.json({ data: null, message: "Account Is Existed" })
-            }
-            // 
-            let result = await UserService.create({ username, password, email, phone, birthdate: birthday });
-            return res.json({ data: result, message: "Register Success" })
-        } catch (error) {
-            logError("Register Error", error)
-            return res.json({ data: error, message: "Register Error" })
-        }
-    },
-    async logout(req, res) {
-        try {
-            res.cookie('token', '', { maxAge: -1 });
-            return res.json({ message: 'Logout Success' })
-        } catch (error) {
-            console.log("Log out error: ", error)
-            return res.json({ message: 'Logout Fail' })
-
-        }
+const { UserService } = require("../services");
+const { logError, logWarn } = require("../utils/index");
+const jwt = require("jsonwebtoken");
+const UserController = {
+  async login(req, res) {
+    try {
+      // Nhận dữ liệu từ request
+      let username = req.body.username;
+      let password = req.body.password;
+      console.log(req.body);
+      let account = await UserService.find({
+        username: username,
+        password: password,
+      });
+      if (account.length == 1) {
+        let user = account[0];
+        let token = jwt.sign(
+          { username: user.username, password: user.password, type: user.type },
+          process.env.SECRET_KEY
+        );
+        res.cookie("token", token, { maxAge: 900000, httpOnly: true });
+        return res.json({ data: account[0], message: "Login success" });
+      } else {
+        logWarn("Login fail", { username: username, password: password });
+        return res.json({ data: null, message: "Account not found" });
+      }
+    } catch (error) {
+      console.log("Login ERROR: ", error);
+      return res.json({ data: error, message: "Login Error" });
     }
-}
+  },
+  async register(req, res) {
+    try {
+      let username = req.body.username;
+      let password = req.body.password;
+      let rePassword = req.body.rePassword;
+      let email = req.body.email;
+      let phone = req.body.phone;
+      let birthday = req.body.birthday;
+      // CHECK EMPTY INPUT  ""
+      if (
+        !username ||
+        !password ||
+        !rePassword ||
+        !email ||
+        !phone ||
+        !birthday
+      ) {
+        console.log("Invalid information", {
+          username,
+          password,
+          rePassword,
+          email,
+          phone,
+        });
+        return res.json({ data: null, message: "Invalid information" });
+      }
+      // CHECK PASSWORD & REPASSWORD
+      else if (password != rePassword) {
+        logWarn("Password not match", {
+          username,
+          password,
+          rePassword,
+          email,
+          phone,
+          birthdate: birthday,
+        });
+        return res.json({ data: null, message: "Password not match" });
+      }
+      // CHECK DUPLICATE ACCOUNT
+      let account = await UserService.find({ username });
+      console.log("Account Found: ", account);
+      if (account.length != 0) {
+        logWarn("Account Is Existed", {
+          username,
+          password,
+          rePassword,
+          email,
+          phone,
+          birthdate: birthday,
+        });
+        return res.json({ data: null, message: "Account Is Existed" });
+      }
+      //
+      let result = await UserService.create({
+        username,
+        password,
+        email,
+        phone,
+        birthdate: birthday,
+      });
+      return res.json({ data: result, message: "Register Success" });
+    } catch (error) {
+      logError("Register Error", error);
+      return res.json({ data: error, message: "Register Error" });
+    }
+  },
+  async logout(req, res) {
+    try {
+      res.cookie("token", "", { maxAge: -1 });
+      return res.json({ message: "Logout Success" });
+    } catch (error) {
+      console.log("Log out error: ", error);
+      return res.json({ message: "Logout Fail" });
+    }
+  },
+  async getUserByUsername(req, res) {
+    try {
+      let username = req.body.username;
+      let data = await UserService.find({ username: username });
+      let user = data[0];
+      return res.json({ data: user });
+    } catch (error) {
+      return res.json(null);
+    }
+  },
+  async updateProfile(req, res) {
+    try {
+      let username = req.body.username;
+      let email = req.body.email;
+      let phone = req.body.phone;
+      let birthdate = req.body.birthdate;
+      let replySyntaxs = req.body.replySyntaxs;
+      console.log(username, email, phone, birthdate)
+      // CHECK EMPTY INPUT  ""
+      if (!username || !email || !phone || !birthdate||!replySyntaxs ) {
+        logWarn("Invalid information", { username, email, phone, birthdate,replySyntaxs });
+        
+        return res.json({ data: null, message: "Invalid information" });
+      }
+      
+      let result = await UserService.updateOne(
+        { username: username },
+        { username, email, phone, birthdate,replySyntaxs }
+      );
+      return res.json({ data: result, message: "Update Success" });
+    } catch (error) {
+      logError("Register Error", error);
+      return res.json({ data: error, message: "Update Error" });
+    }
+  },
+};
 
-
-module.exports = UserController
+module.exports = UserController;
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,33 +1,38 @@
-
-const jwt = require('jsonwebtoken');
-const { UserModel } = require('../models/')
+const jwt = require("jsonwebtoken");
+const { UserModel } = require("../models/");
 // Check token có tồn tại hay không
 async function needLogin(req, res, next) {
-    try {
-        let token = req.cookies.token;
-        let decodeData = jwt.verify(token, process.env.SECRET_KEY)
-        let isValid = await UserModel.find({ username: decodeData.username, password: decodeData.password })
-        if (isValid.length == 1) {
-            next()
-        } else res.redirect('/test/login')
-    } catch (error) {
-        console.log("needLogin Error:", error)
-        res.redirect('/test/login')
-    }
-
+  try {
+    let token = req.cookies.token;
+    let decodeData = jwt.verify(token, process.env.SECRET_KEY);
+    let isValid = await UserModel.find({
+      username: decodeData.username,
+      password: decodeData.password,
+    });
+    req.body.username = decodeData.username;
+    req.body.role = decodeData.role;
+    if (isValid.length == 1) {
+      next();
+    } else res.redirect("/test/login");
+  } catch (error) {
+    console.log("needLogin Error:", error);
+    res.redirect("/test/login");
+  }
 }
 async function needGuest(req, res, next) {
-    let token = req.cookies.token;
-    try {
-        let decodeData = jwt.verify(token, process.env.SECRET_KEY)
-        let isValid = await UserModel.find({ username: decodeData.username, password: decodeData.password })
-        if (isValid.length > 0) {
-            res.redirect('/test/home')
-        }
-        else next()
-    } catch (error) {
-        next()
-    }
+  let token = req.cookies.token;
+  try {
+    let decodeData = jwt.verify(token, process.env.SECRET_KEY);
+    let isValid = await UserModel.find({
+      username: decodeData.username,
+      password: decodeData.password,
+    });
+    if (isValid.length > 0) {
+      res.redirect("/test/home");
+    } else next();
+  } catch (error) {
+    next();
+  }
 }
 
-module.exports = { needLogin, needGuest }
+module.exports = { needLogin, needGuest };
